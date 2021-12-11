4547e7320fde77ed801ae31a8a57ebe9c1e679b1
rpfreitasxyz <rpfreitas@poli.ufrj.br>
2021-12-11 13:35:30 -0300

    feito o script para obter as rodadas do brasileirao A e B

--- teste_partidas.R
+++ teste_partidas.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rvest)
 library(curl)
 library(xlsx)
+library(stringi)
 
 pega_partidas_placar <- function(url) {
   page <- read_html(curl(url, handle = curl::new_handle("useragent" = "Firefox/5.0"))) 
@@ -24,16 +25,6 @@ pega_partidas_placar <- function(url) {
     mutate_at(vars(placar_casa, placar_fora), as.numeric)
 }
 
-# Cria um tibble para mudar os nomes velhos para os novos
-vetor_nomes <- tribble(
-  ~nome_old, ~nome_new,
-  "AMERICA FC - MG", "AMERICA - MG",
-  "ATLETICO - PR", "ATHLETICO PARANAENSE - PR",
-  "ATLETICO PARANAENSE - PR", "ATHLETICO PARANAENSE - PR",
-  "ATLETICO MINEIRO - MG", "ATLETICO - MG",
-  "BRAGANTINO - SP", "RED BULL BRAGANTINO - SP"
-)
-
 # Pega as series do Brasileirao disponiveis
 tab <- tibble(serie = c("A", "B")) %>%
   # Expande para os anos disponiveis
@@ -44,15 +35,29 @@ tab <- tibble(serie = c("A", "B")) %>%
                         edicao)) %>%
   mutate(dados = map(.x = value, .f = pega_partidas_placar))
 
-# TODO: Falta tratar os nomes dos clubes!
+# Cria um tibble para mudar os nomes velhos para os novos
+vetor_nomes <- tribble(
+  ~nome_old, ~nome_new,
+  "A.B.C. - RN", "ABC - RN",
+  "A.S.A. - AL", "ASA - AL",
+  "AMERICA FC - MG", "AMERICA - MG",
+  "ATLETICO - PR", "ATHLETICO PARANAENSE - PR",
+  "ATLETICO PARANAENSE - PR", "ATHLETICO PARANAENSE - PR",
+  "ATLETICO MINEIRO - MG", "ATLETICO - MG",
+  "BRAGANTINO - SP", "RED BULL BRAGANTINO - SP",
+  "C.R.B. - AL", "CRB - AL"
+)
+
 tab_rodadas <- tab %>%
   unnest(dados) %>%
-  mutate(Clube = str_remove(Clube, "[+-]?\\d*") %>% str_trim(side = "both") %>% str_to_upper()) %>%
-  left_join(vetor_nomes, by = c("Clube" = "nome_old")) %>%
-  mutate(Clube = if_else(!is.na(nome_new),
+  select(-value) %>%
+  pivot_longer(cols = c("time_casa", "time_fora"), names_to = "casa_fora", values_to = "time") %>%
+  mutate(time = str_to_upper(time) %>% stri_trans_general("Latin-ASCII")) %>%
+  left_join(vetor_nomes, by = c("time" = "nome_old")) %>%
+  mutate(time = if_else(!is.na(nome_new),
                          nome_new,
-                         Clube)) %>%
-  select(-c(value, nome_new)) %>%
-  arrange(serie, edicao, Posicao)
+                        time)) %>%
+  select(-nome_new) %>%
+  arrange(serie, edicao, rodada)
 
 write.xlsx(tab_rodadas, "rodadas_brasileirao_serie_ab_2012-2021.xlsx")
