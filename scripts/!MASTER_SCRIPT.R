f23ff104cc43f378a51b7f0dfc57f12656611073
rpfreitasxyz <rpfreitas@poli.ufrj.br>
2021-12-14 21:26:24 -0300

    adicionada uma estrutura de pastas ao projeto

---
+++ scripts/!MASTER_SCRIPT.R
@@ -0,0 +1,11 @@
+# Esse script eh o mestre, que coordena a execucao dos outros
+
+# Esse script extrai as tabelas do Brasileirao
+# Series A e B, de 2012 a 2021
+# ESSAS DATAS ESTAO SUJEITAS A MUDANCAS!
+source('scripts/extrai_tabela.R')
+
+# Esse script extrai as partidas do Brasileirao
+# Series A e B, de 2012 a 2021
+# ESSAS DATAS ESTAO SUJEITAS A MUDANCAS!
+source('scripts/extrai_partidas.R')
